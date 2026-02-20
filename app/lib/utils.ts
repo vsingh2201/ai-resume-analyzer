2dfe823445b3af032b1f43333bd2771ef5889035
Vikramjeet Singh <70337807+vsingh2201@users.noreply.github.com>
2026-02-19 19:42:21 -0500

    Resume feedback components

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,11 @@
-// Formats a file size in bytes to a human-readable string (KB, MB, GB)
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
 
-export function formatSize(bytes: number) : string{
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
+
+export function formatSize(bytes: number): string {
     if (bytes === 0) return '0 Bytes';
 
     const k = 1024;
@@ -10,7 +15,7 @@ export function formatSize(bytes: number) : string{
     const i = Math.floor(Math.log(bytes) / Math.log(k));
 
     // Format with 2 decimal places and round
-    return parseFloat((bytes/Math.pow(k,i)).toFixed(2))+ ' ' + sizes[i];
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
 }
 
 export const generateUUID = () => crypto.randomUUID();
